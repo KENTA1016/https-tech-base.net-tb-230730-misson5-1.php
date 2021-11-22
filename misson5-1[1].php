066c6a88a4e450402c61d29aff5ab9378ecf50c1
KENTA1016 <94851554+KENTA1016@users.noreply.github.com>
2021-11-23 00:23:31 +0900

    Add files via upload

---
+++ misson5-1[1].php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>Mission5-1</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+
+    <span style="color:cyan">
+    <h1>あなたの行ってみたい国はどこですか？</h1>
+    </span>
+
+    <hr>
+    
+    <div style="background-color: linen">
+        
+        
+    <form action = "" method = "post">
+        
+        <div class = "list">
+            
+            <input type = "text" name = "name" placeholder = "名前" value = "" >
+            
+            <input type = "text" name = "str" placeholder = "国名" value = "">
+            
+            <input type = "password" name = "pass" placeholder = "パスワード" value = "">
+            
+            <input type = "submit" name = "submitbutton">
+            
+            <input type = "hidden" name = "editing" value = "" >
+            
+        </div>
+        
+    </form>
+    
+    <pre>
+        
+    </pre>
+    
+    削除フォーム
+    <form action = "" method = "post">
+        
+        <dive class = "list">
+            
+            <input type = "number" name = "delete" placeholder = "削除対象番号">
+            
+            <input type = "password" name = "deletepass" placeholder = "パスワード">
+            
+            <input type = "submit" name = "deletebutton" value = "削除">
+            
+        </dive>
+        
+    </form>
+
+    <pre>
+        
+    </pre>
+    
+    編集番号
+    <form action = "" method = "post">
+        
+        <div class = "list">
+            
+            <input type = "number" name = "edit" placeholder = "編集対象番号">
+            
+            <input type = "password" name = "editpass" placeholder = "パスワード">
+            
+            <input type = "submit" name = "editbutton" value = "編集">
+            
+        </div>
+        
+    </form>
+    
+    <hr>
+    
+    1<span style="color:hotpink">KENTA</span>2021/11/22 23:39:06<br><p style = "font-size:25px;">国名：アメリカ</span></p>
+    </font>
+    
+    </body>
+    
+    </div>    
+</html>
